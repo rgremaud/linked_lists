0deab7c36e542134aa691936ccd8d3f4d9af66e3
rgremaud <rgremau@gmail.com>
2026-01-09 21:27:19 -0500

    Add initial JS functions

---
+++ main.js
@@ -0,0 +1,8 @@
+import { LinkedList } from "./lib/linked_list.js"
+
+const linkedList = new LinkedList();
+linkedList.append("Howie");
+linkedList.append("Whitney");
+linkedList.append("Dad");
+linkedList.append("Theo");
+console.log(linkedList.at(0));
