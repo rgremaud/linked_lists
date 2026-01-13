276b68a7646d390bee3ba75c86d3481d516bc6f7
Romain Gremaud <rgremau@gmail.com>
2026-01-13 17:46:45 -0500

    Add example to main.js

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 import { LinkedList } from "./lib/linked_list.js";
 
-const linkedList = new LinkedList();
-linkedList.append("Howie");
-linkedList.append("Whitney");
-linkedList.append("Dad");
-linkedList.append("Theo");
-console.log(linkedList.toString())
-console.log(linkedList.insertAt(4, "GrandP", "Nona"));
-console.log(linkedList.toString())
-// console.log(linkedList.headCheck())
-// console.log(linkedList.removeAt(0));
-// console.log(linkedList.headCheck())
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+console.log(list.toString());
